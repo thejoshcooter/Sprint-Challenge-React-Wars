cd532d90b6a7bd8c72c84b7f7bc898080b70c1ab
josh cooter <thejoshcooter@gmail.com>
2019-04-26 10:41:54 -0500

    add starwarsChar state data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <Cards />
+        <Cards starwarsChars={this.state.starwarsChars} />
       </div>
     );
   }
